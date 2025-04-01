48c85350fa42acce583e4a73e2b316f11e0c9b5b
long89kev <tamlongnguyen1234@gmail.com>
2025-04-01 16:20:48 +0700

    refactor enqueue and dequeue functions for improved process handling

--- src/queue.c
+++ src/queue.c
@@ -10,11 +10,8 @@ int empty(struct queue_t * q) {
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
         /* TODO: put a new process to queue [q] */
         if (q == NULL || proc == NULL) return;
-        if (q->size >= MAX_QUEUE_SIZE) {
-                // Dont know what the fuck to do here
-                return;
-        }
-        q->proc[q->size] = proc;
+        if (q->size >= MAX_QUEUE_SIZE) return; 
+        q->proc[q->size] = proc; 
         q->size++;
 }
 
@@ -22,22 +19,37 @@ struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-        if (q == NULL || empty(q)) return NULL;
-        int highest_priority = q->proc[0]->priority;
-        int highest_index = 0;
-        for (int i = 1; i < q->size; i++) {
-                if (q->proc[i]->priority > highest_priority) {
-                        highest_priority = q->proc[i]->priority;
-                        highest_index = i;
+        
+        #ifdef MLQ_SCHED
+                if (q == NULL || q->size == 0) return NULL;
+
+                struct pcb_t *getProc = malloc(sizeof(struct pcb_t));
+                getProc = q->proc[0];
+
+                for (int i = 0; i < q->size - 1; i++) {
+                        q->proc[i] = q->proc[i + 1];
                 }
-        }
-        struct pcb_t * highest_proc = q->proc[highest_index];
-        for (int i = highest_index; i < q->size - 1; i++) {
-                q->proc[i] = q->proc[i + 1];
-        }
-        q->size--;
-        q->proc[q->size] = NULL; 
-        return highest_proc;
-	return NULL;
+
+                q->size--;
+                return getProc;
+        #else
+                if (q == NULL || q->size == 0) return NULL;
+                struct pcb_t *highest = malloc(sizeof(struct pcb_t));
+                highest = q->proc[0];
+                int highest_index = 0;
+                for (int i = 1; i < q->size; i++) {
+                        if (q->proc[i]->priority < highest->priority) {
+                                highest = q->proc[i];
+                                highest_index = i;
+                        }
+                }
+                for (int i = highest_index; i < q->size - 1; i++) {
+                        q->proc[i] = q->proc[i + 1];
+                }
+                q->size--;
+                return highest;
+        #endif
+
+        return NULL;
 }
 

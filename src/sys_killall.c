71b13aa6fed538df94be0835bbbe1d3fe4bec35c
long89kev <tamlongnguyen1234@gmail.com>
2025-04-02 17:52:19 +0700

    implement process termination in __sys_killall function

--- src/sys_killall.c
+++ src/sys_killall.c
@@ -8,41 +8,69 @@
  * for the sole purpose of studying while attending the course CO2018.
  */
 
-#include "common.h"
-#include "syscall.h"
-#include "stdio.h"
-#include "libmem.h"
-
-int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
-{
-    char proc_name[100];
-    uint32_t data;
-
-    //hardcode for demo only
-    uint32_t memrg = regs->a1;
-    
-    /* TODO: Get name of the target proc */
-    //proc_name = libread..
-    int i = 0;
-    data = 0;
-    while(data != -1){
-        libread(caller, memrg, i, &data);
-        proc_name[i]= data;
-        if(data == -1) proc_name[i]='\0';
-        i++;
-    }
-    printf("The procname retrieved from memregionid %d is \"%s\"\n", memrg, proc_name);
-
-    /* TODO: Traverse proclist to terminate the proc
-     *       stcmp to check the process match proc_name
-     */
-    //caller->running_list
-    //caller->mlq_ready_queu
-
-    /* TODO Maching and terminating 
-     *       all processes with given
-     *        name in var proc_name
-     */
-
-    return 0; 
-}
+ #include "common.h"
+ #include "syscall.h"
+ #include "stdio.h"
+ #include "libmem.h"
+ #include "string.h"
+ 
+ int __sys_killall(struct pcb_t *caller, struct sc_regs* regs)
+ {
+     char proc_name[100];
+     uint32_t data;
+ 
+     //hardcode for demo only
+     uint32_t memrg = regs->a1;
+     
+     /* TODO: Get name of the target proc */
+     //proc_name = libread..
+     int i = 0;
+     data = 0;
+     while(data != -1){
+         libread(caller, memrg, i, &data);
+         proc_name[i]= data;
+         if(data == -1) proc_name[i]='\0';
+         i++;
+     }
+     printf("The procname retrieved from memregionid %d is \"%s\"\n", memrg, proc_name);
+ 
+     /* TODO: Traverse proclist to terminate the proc
+      *       stcmp to check the process match proc_name
+      */
+ 
+     struct pcb_t * proc = NULL;
+     int idx = 0;
+     int pid_to_kill[MAX_PRIO * 10 + 5];
+     int pid_to_kill_count = 0;
+ 
+     while((proc = queue_traversal(caller->running_list, &idx)) != NULL) {
+         if (strcmp(proc->path, proc_name) == 0) {
+             pid_to_kill[pid_to_kill_count++] = proc->pid;
+             break;
+         }
+     }
+ 
+     while((proc = queue_traversal(caller->mlq_ready_queue, &idx)) != NULL) {
+         if (strcmp(proc->path, proc_name) == 0) {
+             pid_to_kill[pid_to_kill_count++] = proc->pid;
+             break;
+         }
+     }
+     
+     //caller->running_list
+     // caller->mlq_ready_queue
+ 
+     /* TODO Maching and terminating 
+      *       all processes with given
+      *        name in var proc_name
+      */
+ 
+     while(pid_to_kill_count > 0) {
+         int pid = pid_to_kill[--pid_to_kill_count];
+         delete(caller->running_list, pid);
+         delete(caller->mlq_ready_queue, pid);
+     }
+ 
+     return 0; 
+ }
+ 
